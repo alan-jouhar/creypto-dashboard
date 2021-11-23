3632a318ec64132b75931eb780653f950b388827
alan-jouhar <alan.jouhar@gmail.com>
2021-11-23 20:16:39 +0100

    Fixed css display for the sidebar in desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import Menu from "./components/menu/menu";
+import Container from "./components/container/container";
 import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<Menu logo={logo}></Menu>
-			<h1 onMouseEnter={() => console.log("color is white")}>
-				This is just Dummy text
-			</h1>
+			<Container>
+				<Menu logo={logo}></Menu>
+				<h1 onMouseEnter={() => console.log("color is white")}>
+					This is just Dummy text
+				</h1>
+			</Container>
 		</div>
 	);
 }
