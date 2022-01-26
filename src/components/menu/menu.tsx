15791ec1c70ea2ee8b930aa4d8cccf4d0a16f943
alan-jouhar <alan.jouhar@gmail.com>
2022-01-27 00:34:03 +0100

    Fixed the main menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,13 +1,33 @@
 import styles from "./menu.module.css";
-import Header from "../header/header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import NavBar from "../nav/nav";
 
 interface MenuProps {
 	logo: string;
+	menuCollapse: boolean;
+	setMenuCollapse: React.Dispatch<React.SetStateAction<boolean>>;
+	className?: string | null;
 }
-function Menu(props: MenuProps): JSX.Element {
+
+function Menu({ logo, menuCollapse, setMenuCollapse, className }: MenuProps) {
+	let [collapsed, setCollapsed] = useState<boolean>(false);
 	return (
-		<section className={styles.menu}>
-			<Header logo={props.logo} />
+		<section className={menuCollapse ? styles.display : styles.menu__header}>
+			<div className={styles.collapse}>
+				<FontAwesomeIcon
+					icon={collapsed ? faTimes : faBars}
+					size="2x"
+					onClick={() => setCollapsed(!collapsed)}
+				/>
+			</div>
+
+			<NavBar collapsed={collapsed} />
+
+			<div className={styles.logo}>
+				<img src={logo} alt="App logo" />
+			</div>
 		</section>
 	);
 }
