365bd2c897f924bd0d35a676efb028e3592fbdb5
alan-jouhar <alan.jouhar@gmail.com>
2022-01-29 23:02:21 +0100

    refactored the walled componenet

--- src/components/wallet/wallet.tsx
+++ src/components/wallet/wallet.tsx
@@ -1,13 +1,31 @@
 import styles from "./wallet.module.css";
 import AddCurrency from "../add_currency/addCurrency";
 import CurrencyWidget, { Currency } from "../currency_widget/currencyWidget";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Wallet() {
 	let [currentSelectedCurrencies, setCurrenSelectedCurrencies] = useState<
 		Currency[]
 	>([]);
+	const loadCurrencies = () => {
+		console.log("I am executing the effect.");
+		var requestOptions: RequestInit = {
+			method: "GET",
+			redirect: "follow",
+		};
 
+		fetch("https://api.coincap.io/v2/assets", requestOptions)
+			.then((response) => response.json())
+			.then((result) => setCurrenSelectedCurrencies(result.data))
+			.catch((error) =>
+				setTimeout(() => {
+					loadCurrencies();
+				}, 3000)
+			);
+	};
+	useEffect(() => {
+		loadCurrencies();
+	}, []);
 	return (
 		<div className={styles.wallet}>
 			{currentSelectedCurrencies.map((curr) => {
